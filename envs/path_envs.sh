a9554039ffd2a26390d39f3ffc2a1dde7d7608e9
Roma <totsamyroma@gmail.com>
2025-11-05 11:24:31 +0300

    Added paths

--- envs/path_envs.sh
+++ envs/path_envs.sh
@@ -1,4 +1,5 @@
 PATH=$HOME/.rbenv/bin:$PATH
+PATH=/opt/homebrew/bin:$PATH
 PATH=/opt/homebrew/opt/libtool/libexec/gnubin:$PATH
 PATH=/opt/homebrew/opt/curl/bin:$PATH
 PATH=/opt/homebrew/opt/mysql@8.4/bin:$PATH
@@ -6,6 +7,9 @@ PATH=/opt/homebrew/opt/sqlite/bin:$PATH
 PATH=/opt/homebrew/opt/postgresql@16/bin:$PATH
 PATH=$(go env GOPATH)/bin:$PATH
 PATH=/Applications/Pritunl.app/Contents/Resources:$PATH
+PATH=$HOME/.dotnet/tools:$PATH
+PATH=$HOME/sources/cast:$PATH
+PATH=$HOME/sources/chromedriver:$PATH
 
 ## mysql@8.4 specific environment variables
 #
