f9ff9128cc3aa1ac3ac8e4228a3c3c2c83e2e3ad
Roma <totsamyroma@gmail.com>
2025-11-05 11:24:41 +0300

    Updated FZF conf

--- envs/fzf.sh
+++ envs/fzf.sh
@@ -1,1 +1,6 @@
-export FZF_COMPLETION_TRIGGER=''
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+export FZF_COMPLETION_TRIGGER='--'
+# export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_DEFAULT_COMMAND='fd --type f --exclude Applications --exclude Library --exclude Movies --exclude Music --exclude Pictures --exclude Public --exclude Desktop'
+
